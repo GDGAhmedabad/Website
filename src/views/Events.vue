aced82b369f05484200aba9fae27d2cd519db09b
Jaldeep Asodariya <jdpatel.android@gmail.com>
2020-05-03 16:51:37 +0530

    Fixes for make build in GCP - Remove package-lock.json from repo and add into gitignore - Revert add changes related to components

--- src/views/Events.vue
+++ src/views/Events.vue
@@ -4,8 +4,8 @@
             <v-row justify="center" align="center" :class="this.$vuetify.theme.dark == true?'grey darken-4':'grey lighten-4'" class="py-0 my-0">
                 <v-col md="12" lg="10" sm="11" xs="12" class="py-3 my-0" >
                     <eventsHeader /> 
-                    <!-- <upcomingMeetupsEvents v-if="checkExistance(config.keysandsecurity.meetup,0)" /> -->
-                    <!-- <featureEvents v-else /> -->
+                    <upcomingMeetupsEvents v-if="checkExistance(config.keysandsecurity.meetup,0)" />
+                    <featureEvents v-else />
                     <featureEvents />
                 </v-col>
             </v-row>
@@ -13,8 +13,8 @@
         <v-container fluid class="pa-0 py-2" >
             <v-row justify="center" align="center">
                 <v-col md="12" lg="10" sm="11" xs="12" class="py-0" >
-                    <!-- <pastMeetupsEvents v-if="checkExistance(config.keysandsecurity.meetup,0)" /> -->
-                    <!-- <customPastEvents v-else/> -->
+                    <pastMeetupsEvents v-if="checkExistance(config.keysandsecurity.meetup,0)" />
+                    <customPastEvents v-else/>
                     <customPastEvents />
                 </v-col>
             </v-row>
@@ -28,8 +28,8 @@ import { mapState } from "vuex";
 export default {
     components:{
         eventsHeader:()=>import('@/components/events/EventHeader'),
-        // upcomingMeetupsEvents:()=>import('@/components/events/UpcomingEvents'),
-        // pastMeetupsEvents:()=>import('@/components/events/PastEvents'),
+        upcomingMeetupsEvents:()=>import('@/components/events/UpcomingEvents'),
+        pastMeetupsEvents:()=>import('@/components/events/PastEvents'),
         featureEvents:()=>import('@/components/home/FeaturesEvents'),
         customPastEvents:()=>import('@/components/events/CustomPastEvents')
     },
